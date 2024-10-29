1683335fcc08d39a9e2420e7ab180d88e75bf6a1
uzwer1 <inthesochi2014@gmail.com>
2024-10-30 00:38:45 +0400

    First commit

---
+++ index.php
@@ -0,0 +1,180 @@
+<!-- 
+Задача: [21-10-24 – 23-10-24] Перевести в SVG лого "Триггер".
+ -->
+
+<!DOCTYPE html>
+<html>
+	<head>
+	    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	    
+	    <meta name="description" content="Прайс-лист строительных, отделочных, монтажных работ от ООО «Триггер» Самарская область (Сергиевский район)">
+	    <meta name="keywords" content="стройка ремонт дом квартира офис работы плиточные отделочные электромонтажные">
+	    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	    <title>Прайс на ремонтно-отделочные работы в Самарской области (Сергиевский р-он)</title>
+	    <meta http-equiv= "X-UA-Compatible" content="IE=edge">
+	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	    <link type="text/css" rel="stylesheet" href="css/style.css">
+	    <link rel="stylesheet" href="css/font-awesome.min.css">
+	    
+	    
+	 </head>
+	<body>
+
+<div class="fon">
+
+<!-- <a href="gallery.html"><img class="icon" src="imags/i.jpg" width="30px" alt="#"></a> <br />-->
+
+
+<h1>Прайс на ремонтно-отделочные работы в Самарской области (Сергиевский р-он)</h1>
+
+
+
+
+<!-- <audio src="audio/2.mp3" autoplay="true" loop="true" preload="true">
+Браузер не поддерживает audio
+</audio> -->
+
+
+<h4><i>Самара - <?php echo date("d/m/Y");?></i></h4>
+
+
+<nav class="nav">
+  <a href="gallery.html"><i class="fa fa-image fa-5x"></i></a>
+  <a href="map-ya.html" class="ya-map"><i class="fa fa-map-marker fa-5x"></i></a>
+  <a href="gallery-carousel.html"><i class="fa fa-play-circle-o fa-5x"></i></a>
+</nav>
+
+<div class="t-pri">
+
+<table class="box shadow">
+
+<tr class="cat">
+	<th>
+		<img src="../imags/ico/86365.png" alt="">
+	</th>
+	<th>Штукатурно-малярные</br> и общестроительные работы</th>
+	<th>ед.измер</th><th>расценка</th>
+</tr>
+Lorem ipsum (dolor sit amet consectetur adipisicing elit. Rerum ut esse, necessitatibus atque, nisi sapiente placeat neque) mollitia obcaecati voluptas dignissimos dicta earum recusandae rem nesciunt id nostrum animi! Sint.
+<tr><td></td><td>Штукатурка <strong>откосов</strong> с установкой малярного уголка</td><td>м.п</td><td>550р.</td></tr>
+<tr><td></td><td><span class="str">Штукатурка</span> стен по маякам толщиной менее 3 см</td><td>кв.м</td><td>500р.</td></tr>
+<tr><td></td><td>Штукатурка стен под шпатель</td><td>кв.м</td><td>250р.</td></tr>
+<tr><td></td><td>Штукатурка стен по маякам толщиной более 3 см</td><td>кв.м</td><td>600р.</td></tr>
+<tr><td></td><td>Грунтовка стен и потолка</td><td>кв.м</td><td>40р.</td></tr>
+<tr><td></td><td>Грунтовка стен и потолка спец.грунтом или клеем</td><td>кв.м</td><td>70р.</td></tr>
+<tr><td></td><td>Выравнивание стен и потолка визуально (плоскость)</td><td>кв.м</td><td>250-300р.</td></tr>
+<tr><td></td><td><strong>Шпатлевка стен</strong> под обои</td><td>кв.м</td><td>200р.</td></tr>
+<tr><td></td><td><strong>Шпатлевка потолка</strong> под обои</td><td>кв.м</td><td>250р.</td></tr>
+<tr><td></td><td>Шпатлевка <strong>финишная стен</strong> под окраску</td><td>кв.м</td><td>120р.</td></tr>
+<tr><td></td><td>Шпатлевка <strong>финишная потолка</strong> под окраску</td><td>кв.м</td><td>150р.</td></tr>
+<tr><td></td><td>Шпатлевка вертикальных "элементов"</td><td>м.п</td><td>150р.</td></tr>
+<tr><td></td><td><span class="str">Шпатлевка откосов</span></td><td>м.п</td><td>200р.</td></tr>
+<tr><td></td><td>Оклейка стен <span class="str">обоями</span></td><td>кв.м</td><td>200-300р.</td></tr>
+<tr><td></td><td>Наклейка обойного фриза</td><td>м.п</td><td>150р.</td></tr>
+<tr><td></td><td>Оклейка потолка обоями</td><td>кв.м</td><td>250-350р.</td></tr>
+<tr><td></td><td>Окраска стен</td><td>кв.м</td><td>130-180р.</td></tr>
+<tr><td></td><td>Окраска потолка</td><td>кв.м</td><td>120-180р.</td></tr>
+<tr><td></td><td>Окраска откосов</td><td>м.п</td><td>150р.</td></tr>
+<tr><td></td><td>Декоративная отделка стен (декоративные краски, флоки, декоративные шпатлевки)</td><td>кв.м</td><td>400-800р.</td></tr>
+<tr><td></td><td>Античные покрытия</td><td>кв.м</td><td>500-1000р.</td></tr>
+<tr><td></td><td>Установка малярных уголков</td><td>м.п</td><td>100р.</td></tr>
+<tr><td></td><td>Наклейка сетки "Строби"</td><td>кв.м</td><td>150р.</td></tr>
+<tr><td></td><td>Монтаж потолочного багета</td><td>м.п</td><td>100/120/150р.</td></tr>
+<tr><td></td><td>Окраска потолочного багета</td><td>м.п</td><td>60/80/100р.</td></tr>
+<tr><td></td><td>Устройство стяжки (цемент/песок/пластификатор) толщиной менее 7 см</td><td>кв.м</td><td>500р.</td></tr>
+<tr><td></td><td>Устройство стяжки (цемент/песок/пластификатор) толщиной более 7 см</td><td>кв.м</td><td>800р.</td></tr>
+<tr><td></td><td>Устройство стяжки (цемент/песок/пластификатор/керамзит) толщиной более 7 см</td><td>кв.м</td><td>700р.</td></tr>
+<tr><td></td><td>Армирование стяжки (сетка Штрек, демпф.лента)</td><td>кв.м</td><td>150р.</td></tr>
+</table>
+
+
+
+<table class="box shadow">
+<tr class="cat"><th><img src="../imags/ico/46592.png" alt=""></th><th>Гипсокартон</th><th></th><th></th></tr>
+<tr><td></td><td>Монтаж ГКЛ на стены по профилю</td><td>кв.м</td><td>500р.</td></tr>
+<tr><td></td><td>Оклеивание стен ГКЛ</td><td>кв.м</td><td>450р.</td></tr>
+<tr><td></td><td>Монтаж ГКЛ на криволинейные стены</td><td>кв.м</td><td>750р.</td></tr>
+<tr><td></td><td>Нашивание второго слоя ГКЛ на стены</td><td>кв.м</td><td>300р.</td></tr>
+<tr><td></td><td>Перегородки из ГКЛ 2хсторонние, прямые</td><td>кв.м</td><td>900р.</td></tr>
+<tr><td></td><td>Перегородки из ГКЛ 2хсторонние, криволинейные</td><td>кв.м</td><td>1200р.</td></tr>
+<tr><td></td><td>Шумоизоляция стен, перегородок (полистирол/минвата/пена)</td><td>кв.м</td><td>150р.</td></tr>
+<tr><td></td><td>Изготовление откосов из ГКЛ</td><td>м.п</td><td>300р.</td></tr>
+<tr><td></td><td>Утепление откосов из ГКЛ</td><td>м.п</td><td>80р.</td></tr>
+<tr><td></td><td>Потолки ровные из ГКЛ</td><td>кв.м</td><td>500р.</td></tr>
+<tr><td></td><td>Декоративные отступы (короба) из ГКЛ &lt; 60см</td><td>м.п</td><td>600р.</td></tr>
+<tr><td></td><td>Потолки сложные с прямыми линиями</td><td>кв.м</td><td>750р.</td></tr>
+<tr><td></td><td>Потолки сложные с криволинейными поверхностями</td><td>кв.м</td><td>1000р.-1500р.</td></tr>
+<tr><td></td><td>Ниши для гардин</td><td>м.п</td><td>750р.</td></tr>
+<tr><td></td><td>Ниши, козырьки</td><td>кв.м</td><td>1000р.-1500р.</td></tr>
+<tr><td></td><td>Арки, полуарки</td><td>шт</td><td>от 3000р.</td></tr>
+</table>
+
+
+
+<table class="box shadow">
+<tr class="cat"><th><img src="../imags/ico/33948.png" alt=""></th><th>Кафель</th><th></th><th></th></tr>
+<tr><td></td><td>Подготовка поверхности: грунтовка,</td><td>кв.м</td><td>50р.</td></tr>
+<tr><td></td><td>зачистка,</td><td>кв.м</td><td>50р.</td></tr>
+<tr><td></td><td>(при необходимости) гидроизоляция</td><td>кв.м</td><td>250р.</td></tr>
+<tr><td></td><td>Простая плитка (полы, стены)</td><td>кв.м</td><td>1200-1500р.</td></tr>
+<tr><td></td><td>Крупноформатная плитка 60x60см, 60х120см (полы, стены)</td><td>кв.м</td><td>1500-2000р.</td></tr>
+<tr><td></td><td>Мелкоформатная плитка 10х10см (полы, стены) </td><td>кв.м</td><td>2000-2500р.</td></tr>
+<tr><td></td><td>Мозаичные работы из кафельной плитки</td><td>кв.м</td><td>2500-3500р.</td></tr>
+<tr><td></td><td>Работы по криволинейным плоскостям</td><td></td><td>К=1,3</td></tr>
+<tr><td></td><td>Облицовка ниш, колонн, площадью до 3-х кв.м</td><td>кв.м</td><td>2000р.</td></tr>
+<tr><td></td><td>Затирка швов обычной плитки</td><td>кв.м</td><td>150р.</td></tr>
+<tr><td></td><td>Затирка швов плитки 10х10см и менее</td><td>кв.м</td><td>200р.</td></tr>
+<tr><td></td><td>Затирка швов мозаичной плитки</td><td>кв.м</td><td>300р.</td></tr>
+<tr><td></td><td>Установка молдингов</td><td>м.п</td><td>200р.</td></tr>
+<tr><td></td><td>Установка фризов</td><td>м.п</td><td>300р.</td></tr>
+<tr><td></td><td>Изготовление плинтуса из плитки (подрезка)</td><td>м.п</td><td>300р.</td></tr>
+<tr><td></td><td>Установка керамического плинтуса</td><td>м.п</td><td>300р.</td></tr>
+</table>
+
+
+
+<table class="box shadow">
+<tr class="cat"><th></th><th>Плотницко-столярные работы</th><th></th><th></th></tr>
+<tr><td></td><td>Укладка фанеры на пол</td><td>кв.м</td><td>350р.</td></tr>
+<tr><td></td><td>Укладка и раскрой подложки</td><td>кв.м</td><td>70р.</td></tr>
+<tr><td></td><td>Укладка ламинированного паркета (доски)</td><td>кв.м</td><td>350р.-500р.</td></tr>
+<tr><td></td><td>Отделка стен дерев.вагонкой</td><td>кв.м</td><td>800р.</td></tr>
+<tr><td></td><td>Отделка потолков дерев.вагонкой</td><td>кв.м</td><td>1000р.</td></tr>
+<tr><td></td><td>Установка плинтусов</td><td>м.п</td><td>120р.</td></tr>
+<tr><td></td><td>Монтаж подоконника</td><td>м.п</td><td>500р.</td></tr>
+</table>
+
+
+
+<table class="box shadow">
+<tr class="cat"><th></th><th>Разное</th><th></th><th></th></tr>
+<tr><td></td><td>Монтаж потолка "Армстронг" простого</td><td>кв.м</td><td>500р.</td></tr>
+<tr><td></td><td>Монтаж потолка "Армстронг" сложного</td><td>кв.м</td><td>700р.</td></tr>
+<tr><td></td><td>Монтаж подвесного потолка из панелей ПВХ</td><td>кв.м</td><td>500р.</td></tr>
+<tr><td></td><td>Монтаж порогов</td><td>шт.</td><td>300р.</td></tr>
+<tr><td></td><td>Устройство наливных полов</td><td>кв.м</td><td>250р.</td></tr>
+<tr><td></td><td>Укладка, раскрой, прирезка линолеума, ковролина</td><td>кв.м</td><td>300р.</td></tr>
+<tr><td></td><td>Наклейка линолеума, ковролина</td><td>кв.м</td><td>100р.</td></tr>
+<tr><td></td><td>Лакировка вагонки 1 слой</td><td>кв.м</td><td>200р.</td></tr>
+<tr><td></td><td>Лакировка вагонки 2 слой</td><td>кв.м</td><td>100р.</td></tr>
+<tr><td></td><td>Лакировка деревянного плинтуса, наличника в 2 слоя</td><td>м.п</td><td>200р.</td></tr>
+</table>
+
+<br>
+
+<!-- <img src="./imags/Clear Card.svg" width="400"alt=""> -->
+
+
+</div>
+
+</div>
+<footer class="footer">
+    <!-- Copyright © 2000–/*<?php echo date("Y");?>*/ «Триггер» -->
+    <div id="mpurrep"><img src="imags/mpurrep0.png" alt="">
+    <span id="cur_year"><?php echo date("Y");?></span>
+    </div>
+</footer>
+</body>
+</html>
